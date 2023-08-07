ce547b04b4c0a8051643c1817c21fdb55aa92588
axelthepony27 <axeldgarciab@gmail.com>
2023-08-07 14:14:33 -0500

    Adds ToDo and Priority classes.
    This commit adds the model/ directory, which contains the DTO's for the project. We also add the Priorrity.java and the ToDo.java DTO classes, which model the behaviour of the to-to entity in the project.

---
+++ src/main/java/com/example/todo_list_backend/model/Priority.java
@@ -0,0 +1,26 @@
+package com.example.todo_list_backend.model;
+
+import java.awt.*;
+
+public enum Priority {
+    LOW ("Low", Color.GREEN),
+    MEDIUM("Medium", Color.YELLOW),
+    HIGH ("High", Color.RED);
+
+    private final String description;
+    private final Color color;
+
+    Priority(String description, Color color) {
+        this.description = description;
+        this.color = color;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public Color color(){
+        return color;
+    }
+
+}
