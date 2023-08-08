bfcf38ad1e325d70564211a8bb8702e4f28556b5
axelthepony27 <axeldgarciab@gmail.com>
2023-08-07 19:47:54 -0500

    Adds GET endpoint for a single ToDo.
    This commit adds a GET endpoint method getToDo(), which returns a ToDo from the repositopry given its id, or throws a ToDoNotFoundException if not found.

--- src/main/java/com/example/todo_list_backend/controller/ToDoController.java
+++ src/main/java/com/example/todo_list_backend/controller/ToDoController.java
@@ -3,6 +3,7 @@ package com.example.todo_list_backend.controller;
 import com.example.todo_list_backend.model.ToDo;
 import com.example.todo_list_backend.repository.ToDoRepositoryImpl;
 import com.example.todo_list_backend.service.ToDoServiceImpl;
+import com.example.todo_list_backend.service.errorHandling.ToDoNotFoundException;
 import com.example.todo_list_backend.utils.JsonHandler;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,15 @@ public class ToDoController {
     @GetMapping("")
     private ResponseEntity<String> toDos() {
         return new ResponseEntity<>(JsonHandler.toJson(toDoRepository.toDoMap), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<String> getToDo(@PathVariable int id) {
+        if(toDoRepository.existsById(id)) {
+            return new ResponseEntity<>(JsonHandler.toJson(toDoRepository.findById(id)), HttpStatus.OK);
+        }else {
+            throw new ToDoNotFoundException(String.format("Couldn't find ToDo with ID: %d", id));
+        }
     }
 
     @PostMapping("")
