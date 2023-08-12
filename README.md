5728493f23b85c19fd33574a9133de3e0b66be8d
axelthepony27 <axeldgarciab@gmail.com>
2023-08-11 19:12:28 -0500

    Changes the port of the front end to localhost:8080.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # To-Do_List_Backend
+## How to run
+To run the application, run the following command on the console:
+```mvn spring-boot:run```
+
+To test the application, run:
+```mvn test```
--- src/main/java/com/example/todo_list_backend/ToDoListBackendApplication.java
+++ src/main/java/com/example/todo_list_backend/ToDoListBackendApplication.java
@@ -2,11 +2,12 @@ package com.example.todo_list_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ToDoListBackendApplication {
 
     public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class ToDoListBackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/todos").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/todos").allowedOrigins("http://localhost:8080");
             }
         };
     }
