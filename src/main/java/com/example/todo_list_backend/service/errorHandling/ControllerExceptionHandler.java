fe146c2bd2b094697ee1d370f13c47c16ab64ff1
axelthepony27 <axeldgarciab@gmail.com>
2023-08-16 14:43:56 -0600

    Overrides method handleMethodArgumentNotValid() in the ControllerAdvice.
    This commit overrides the method handleMethodArgumentNotValid() in the ContollerAdvice class ControllerExceptionHandler.java, wich handles the exception MethodArgumentNotValidException. This exception is thrown when validation on an argument annotated with @Valid fails.

--- src/main/java/com/example/todo_list_backend/service/errorHandling/ControllerExceptionHandler.java
+++ src/main/java/com/example/todo_list_backend/service/errorHandling/ControllerExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.example.todo_list_backend.service.errorHandling;
 
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -18,11 +24,25 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
             WebRequest request) {
         CustomErrorMessage body = new CustomErrorMessage(LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
-                e.getCause().getMessage(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
                 e.getMessage(),
                 request.getDescription(false)
                 );
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request){
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", status.value());
+        body.put("timestamp", LocalDateTime.now());
+        body.put("exception", ex.getClass());
+        body.put("detail", ex.getBody().getDetail());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, headers, status);
     }
 
 }
